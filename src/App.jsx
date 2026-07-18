29f79efcca2eb7b4f089ae120342153211d45315
rcayroc02 <roberto.cayro@ucsp.edu.pe>
2026-07-18 10:21:19 -0500

    LogIn basico - screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import DashboardLayout from './components/DashboardLayout'
 import Resumen from './components/Resumen'
 import Transacciones from './components/Transacciones'
 import MisCreditos from './components/MisCreditos'
+import { Login } from './components/LogIn'
+import PrivateRoute from './components/session/PrivateRoute'
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route element={<DashboardLayout />}>
+      {/* Ruta pública */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Rutas protegidas */}
+      <Route
+        element={
+          <PrivateRoute>
+            <DashboardLayout />
+          </PrivateRoute>
+        }
+      >
         <Route path="/" element={<Navigate to="/resumen" replace />} />
         <Route path="/resumen" element={<Resumen />} />
         <Route path="/transacciones" element={<Transacciones />} />
         <Route path="/mis-creditos" element={<MisCreditos />} />
       </Route>
+
+      {/* Cualquier ruta no encontrada */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
 }
